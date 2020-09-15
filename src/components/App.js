62d02ab6d5731af15ae370777bd7163879bc0042
chsohn15 <chsohn15@gmail.com>
2020-09-15 15:06:02 -0400

    built filter feature

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,16 @@ class App extends React.Component {
     }
   }
 
+  onChangeType = (e) => {
+
+    this.setState({
+      filters:{
+        type: e.target.value
+      }
+    })
+  }
+
+  
   render() {
     return (
       <div className="ui container">
@@ -24,7 +34,7 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.onChangeType}/>
             </div>
             <div className="twelve wide column">
               <PetBrowser />
